6ada5bd301af58790f52eb2e4bf829b59f0b5cf2
Zachary Blackwood <zachary@streamlit.io>
2024-01-22 21:17:16 +0000

    Back to is_section

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ COLOR_SHARED = "#fc8d59"
 # Streamlit app
 #####
 show_pages_from_config()
-add_page_title(page_title="Home", layout="wide")
+add_page_title(page_title="Home", layout="wide", add_indentation=False)
 
 st.stop()
 
